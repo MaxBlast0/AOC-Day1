6ec065d1b57b3e697394794d215c64118856f4b1
MaxBlast0 <71572314+MaxBlast0@users.noreply.github.com>
2022-12-18 01:47:08 -0500

    Solved!

--- Program.cs
+++ Program.cs
@@ -14,12 +14,34 @@ namespace AOC_Day1
                 using (StreamReader sr = new StreamReader("TestFile.txt"))
                 {
                     string line;
+                    int highestCalorieSum = int.MinValue;
+                    int currentCalorieSum = 0;
+                    int currentLine = 0;
+                    bool convertToInt;
+                   
+
                     // Read and display lines from the file until the end of
                     // the file is reached.
+
                     while ((line = sr.ReadLine()) != null)
                     {
-                        Console.WriteLine(line);
+                        convertToInt = int.TryParse(line, out currentLine); // Convert parsed string into an integer
+                        currentCalorieSum = currentCalorieSum + currentLine; // Add parsed integer to current calorie sum
+                       
+                        if (string.IsNullOrWhiteSpace(line))  // If statement to see if line is blank and if so check against highest number
+                        {
+                            if (currentCalorieSum > highestCalorieSum)
+                            {
+                                highestCalorieSum = currentCalorieSum;
+                               
+                            }
+                            currentCalorieSum = 0; //reset currentCalorieSum for next iteration
+                        }
+
+                       
+                        
                     }
+                    Console.WriteLine("The highest Calorie sum is: " + highestCalorieSum);
                 }
             }
             catch (Exception e)
@@ -28,6 +50,7 @@ namespace AOC_Day1
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+           
         }
     }
 }
